67a4115965f8445f8f382cecf1c81e6057a677ed
Simo Parvanov <simo86java@gmail.com>
2023-07-30 19:20:12 +0300

    first commit

---
+++ src/main/java/com/example/sptingapp/SptingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sptingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SptingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sptingapp/SptingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sptingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
